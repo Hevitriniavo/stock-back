ac57105e88f53914e6ae9f42884876cb636489de
Hevitriniavo <hei.tantely@gmail.com>
2024-02-06 19:05:27 +0300

    fix: created migration and model of the class order line

---
+++ app/Models/OrderLine.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class OrderLine extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'article_id',
+        'order_id',
+        'selling_price',
+        'quantity',
+    ];
+
+    public function article(): BelongsTo
+    {
+        return $this->belongsTo(Article::class);
+    }
+
+    public function order(): BelongsTo
+    {
+        return $this->belongsTo(Order::class);
+    }
+}
