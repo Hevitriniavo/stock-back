3f88497ae2eda2692540313d561c0af1b46c9b8e
Hevitriniavo <hei.tantely@gmail.com>
2024-02-06 18:32:44 +0300

    fix: added relation many to one entrer two class customer and phoneNumber

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Customer extends Model
 {
@@ -17,5 +18,10 @@ class Customer extends Model
         'city',
     ];
 
+
+    public function phoneNumbers(): HasMany
+    {
+        return $this->hasMany(PhoneNumber::class);
+    }
 
 }
--- app/Models/PhoneNumber.php
+++ app/Models/PhoneNumber.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class PhoneNumber extends Model
 {
@@ -14,4 +15,10 @@ class PhoneNumber extends Model
         'customer_id',
         'supplier_id',
     ];
+
+
+    public function customer(): BelongsTo
+    {
+        return $this->belongsTo(Customer::class);
+    }
 }
