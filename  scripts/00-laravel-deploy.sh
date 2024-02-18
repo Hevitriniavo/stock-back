55901f09d1381937b713f750b2e79f1bf233b3fb
Hevitriniavo <hei.tantely@gmail.com>
2024-02-18 12:15:54 +0300

    created docker file

---
+++  scripts/00-laravel-deploy.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+# Set permissions for storage directory
+chmod -R  775 /var/www/html/storage
+
+# Copy environment file
+echo "Running composer"
+cp /etc/secrets/.env .env
+
+# Install dependencies
+composer global require hirak/prestissimo
+composer install --no-dev --working-dir=/var/www/html
+
+# Create symbolic link for public storage
+php artisan storage:link
+
+# Clear caches
+echo "Clearing caches..."
+php artisan optimize:clear
+
+# Cache configurations and routes
+echo "Caching config..."
+php artisan config:cache
+
+echo "Caching routes..."
+php artisan route:cache
+
+# Run migrations
+echo "Running migrations..."
+php artisan migrate
+
+echo "done deploying"
